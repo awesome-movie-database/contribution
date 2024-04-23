0faec091423b1935027e95481cbd21cf5d1a6a2e
Madnoberson <baseddepartmentzx77@gmail.com>
2024-04-23 14:15:35 +0400

    Add `Maybe`

---
+++ src/contribution/domain/__init__.py
@@ -0,0 +1,8 @@
+from .constants import *
+from .entities import *
+from .exceptions import *
+from .services import *
+from .validators import *
+from .value_objects import *
+
+from .maybe import Maybe as Maybe
---
+++ src/contribution/domain/maybe.py
@@ -0,0 +1,53 @@
+from typing import TypeVar, Generic, Union, cast
+from enum import IntEnum, auto
+
+
+_T = TypeVar("_T")
+
+
+class _Unset(IntEnum):
+    UNSET = auto()
+
+
+class Maybe(Generic[_T]):
+    """
+    Class for representing value that can be
+    not set.
+
+    Basic usage::
+
+        class Foo:
+            def __init__(self, bar: Optional[int]):
+                self.bar = bar
+
+        foo = Foo(1)
+
+        def update_foo(bar: Maybe[Optional[int]]):
+            if bar.is_set():
+                foo.bar = bar
+    """
+
+    def __init__(self, value: Union[_T, _Unset]) -> None:
+        self._value = value
+
+    @classmethod
+    def without_value(cls) -> "Maybe":
+        return Maybe(_Unset.UNSET)
+
+    @classmethod
+    def with_value(cls, value: _T) -> "Maybe":
+        return Maybe(value)
+
+    @property
+    def value(self) -> _T:
+        """
+        Returns value if value is set, otherwise
+        raises ValueError.
+        """
+        if not self.is_set():
+            message = "Value is unset"
+            raise ValueError(message)
+        return cast(_T, self.value)
+
+    def is_set(self) -> bool:
+        return not isinstance(self._value, _Unset)
