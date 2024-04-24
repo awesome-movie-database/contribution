d6938aba0e838f74e6ed80fe8eb3061746b35350
Madnoberson <baseddepartmentzx77@gmail.com>
2024-04-24 18:03:49 +0400

    Add `CreateWriter` domain service

---
+++ src/contribution/domain/services/create_writer.py
@@ -0,0 +1,24 @@
+from contribution.domain.constants import Writing
+from contribution.domain.value_objects import WriterId
+from contribution.domain.entities import (
+    Writer,
+    Movie,
+    Person,
+)
+
+
+class CreateWriter:
+    def __call__(
+        self,
+        *,
+        id: WriterId,
+        movie: Movie,
+        person: Person,
+        writing: Writing,
+    ) -> Writer:
+        return Writer(
+            id=id,
+            movie_id=movie.id,
+            person_id=person.id,
+            writing=writing,
+        )
