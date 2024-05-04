4e80e5c9c2579026110bcb1b91e3872fc2ce74e4
Madnoberson <baseddepartmentzx77@gmail.com>
2024-05-04 22:56:03 +0400

    Improve logging for `EditMovieProcessor`

--- src/contribution/application/command_processors/edit_movie.py
+++ src/contribution/application/command_processors/edit_movie.py
@@ -10,6 +10,7 @@ from contribution.domain.value_objects import (
     WriterId,
     CrewMemberId,
 )
+from contribution.domain.exceptions import UserIsNotActiveError
 from contribution.domain.services import EditMovie
 from contribution.application.common.services import (
     AccessConcern,
@@ -27,6 +28,7 @@ from contribution.application.common.exceptions import (
     RolesDoNotExistError,
     WritersDoNotExistError,
     CrewMembersDoNotExistError,
+    PersonsDoNotExistError,
 )
 from contribution.application.common.gateways import (
     EditMovieContributionGateway,
@@ -64,6 +66,8 @@ def edit_movie_factory(
     identity_provider: IdentityProvider,
     on_movie_edited: OnMovieEdited,
 ) -> CommandProcessor[EditMovieCommand, EditMovieContributionId]:
+    current_timestamp = datetime.now(timezone.utc)
+
     edit_movie_processor = EditMovieProcessor(
         edit_movie=edit_movie,
         ensure_persons_exist=ensure_persons_exist,
@@ -76,7 +80,7 @@ def edit_movie_factory(
         crew_member_gateway=crew_member_gateway,
         object_storage=object_storage,
         identity_provider=identity_provider,
-        on_movie_edited=on_movie_edited,
+        current_timestamp=current_timestamp,
     )
     authz_processor = AuthorizationProcessor(
         processor=edit_movie_processor,
@@ -84,12 +88,20 @@ def edit_movie_factory(
         permissions_gateway=permissions_gateway,
         identity_provider=identity_provider,
     )
-    tx_processor = TransactionProcessor(
+    callback_processor = CallbackProcessor(
         processor=authz_processor,
+        create_photo_from_obj=create_photo_from_obj,
+        identity_provider=identity_provider,
+        on_movie_edited=on_movie_edited,
+        current_timestamp=current_timestamp,
+    )
+    tx_processor = TransactionProcessor(
+        processor=callback_processor,
         unit_of_work=unit_of_work,
     )
     log_processor = LoggingProcessor(
         processor=tx_processor,
+        identity_provider=identity_provider,
     )
 
     return log_processor
@@ -110,7 +122,7 @@ class EditMovieProcessor:
         crew_member_gateway: CrewMemberGateway,
         object_storage: ObjectStorage,
         identity_provider: IdentityProvider,
-        on_movie_edited: OnMovieEdited,
+        current_timestamp: datetime,
     ):
         self._edit_movie = edit_movie
         self._ensure_persons_exist = ensure_persons_exist
@@ -123,14 +135,13 @@ class EditMovieProcessor:
         self._crew_member_gateway = crew_member_gateway
         self._object_storage = object_storage
         self._identity_provider = identity_provider
-        self._on_movie_edited = on_movie_edited
+        self._current_timestamp = current_timestamp
 
     async def process(
         self,
         command: EditMovieCommand,
     ) -> EditMovieContributionId:
         current_user_id = await self._identity_provider.user_id()
-        current_timestamp = datetime.now(timezone.utc)
 
         author = await self._user_gateway.with_id(current_user_id)
         if not author:
@@ -153,7 +164,6 @@ class EditMovieProcessor:
         add_photos = [
             self._create_photo_from_obj(obj) for obj in command.add_photos
         ]
-        add_photos_urls = [photo.url for photo in add_photos]
 
         contribution = self._edit_movie(
             id=EditMovieContributionId(uuid7()),
@@ -173,34 +183,12 @@ class EditMovieProcessor:
             remove_writers=command.remove_writers,
             add_crew=command.add_crew,
             remove_crew=command.remove_crew,
-            add_photos=add_photos_urls,
-            current_timestamp=current_timestamp,
+            add_photos=[photo.url for photo in add_photos],
+            current_timestamp=self._current_timestamp,
         )
         await self._edit_movie_contribution_gateway.save(contribution)
 
         await self._object_storage.save_photo_seq(add_photos)
-
-        await self._on_movie_edited(
-            id=contribution.id,
-            author_id=current_user_id,
-            movie_id=command.movie_id,
-            title=command.title,
-            release_date=command.release_date,
-            countries=command.countries,
-            genres=command.genres,
-            mpaa=command.mpaa,
-            duration=command.duration,
-            budget=command.budget,
-            revenue=command.revenue,
-            add_roles=command.add_roles,
-            remove_roles=command.remove_roles,
-            add_writers=command.add_writers,
-            remove_writers=command.remove_writers,
-            add_crew=command.add_crew,
-            remove_crew=command.remove_crew,
-            add_photos=add_photos_urls,
-            edited_at=current_timestamp,
-        )
 
         return contribution.id
 
@@ -270,30 +258,162 @@ class EditMovieProcessor:
             raise CrewMembersDoNotExistError(list(ids_of_missing_crew_members))
 
 
-class LoggingProcessor:
-    def __init__(self, processor: TransactionProcessor):
+class CallbackProcessor:
+    def __init__(
+        self,
+        *,
+        processor: AuthorizationProcessor,
+        create_photo_from_obj: CreatePhotoFromObj,
+        identity_provider: IdentityProvider,
+        on_movie_edited: OnMovieEdited,
+        current_timestamp: datetime,
+    ):
         self._processor = processor
+        self._create_photo_from_obj = create_photo_from_obj
+        self._identity_provider = identity_provider
+        self._on_movie_edited = on_movie_edited
+        self._current_timestamp = current_timestamp
 
     async def process(
         self,
         command: EditMovieCommand,
     ) -> EditMovieContributionId:
-        logger.debug(
-            msg="Processing Edit Movie command",
-            extra={"command": command},
+        result = await self._processor.process(command)
+        current_user_id = await self._identity_provider.user_id()
+        add_photos = [
+            self._create_photo_from_obj(obj) for obj in command.add_photos
+        ]
+
+        await self._on_movie_edited(
+            id=result,
+            author_id=current_user_id,
+            movie_id=command.movie_id,
+            title=command.title,
+            release_date=command.release_date,
+            countries=command.countries,
+            genres=command.genres,
+            mpaa=command.mpaa,
+            duration=command.duration,
+            budget=command.budget,
+            revenue=command.revenue,
+            add_roles=command.add_roles,
+            remove_roles=command.remove_roles,
+            add_writers=command.add_writers,
+            remove_writers=command.remove_writers,
+            add_crew=command.add_crew,
+            remove_crew=command.remove_crew,
+            add_photos=[photo.url for photo in add_photos],
+            edited_at=self._current_timestamp,
         )
+
+        return result
+
+
+class LoggingProcessor:
+    def __init__(
+        self,
+        *,
+        processor: TransactionProcessor,
+        identity_provider: IdentityProvider,
+    ):
+        self._processor = processor
+        self._identity_provider = identity_provider
+
+    async def process(
+        self,
+        command: EditMovieCommand,
+    ) -> EditMovieContributionId:
+        current_user_id = await self._identity_provider.user_id()
+        command_processing_id = uuid7()
+
+        logger.debug(
+            "'Edit Movie' command processing started",
+            extra={
+                "processing_id": command_processing_id,
+                "command": command,
+                "current_user_id": current_user_id,
+            },
+        )
+
         try:
             result = await self._processor.process(command)
         except UserDoesNotExistError as e:
-            logger.error(
-                msg="User is authenticated, but user gateway returns None",
-                extra={"user_id": e.id},
+            logger.warning(
+                "Unexpected error occurred: "
+                "User is authenticated, but user gateway returns None",
+                extra={"processing_id": command_processing_id},
+            )
+            raise e
+        except MovieDoesNotExistError as e:
+            logger.debug(
+                "Expected error occurred: Movie doesn't exist",
+                extra={"processing_id": command_processing_id},
+            )
+            raise e
+        except UserIsNotActiveError as e:
+            logger.debug(
+                "Expected error occurred: User is not active",
+                extra={"processing_id": command_processing_id},
+            )
+            raise e
+        except RolesDoNotExistError as e:
+            logger.debug(
+                "Expected error occurred: "
+                "Roles ids entered by user do not belong to any roles",
+                extra={
+                    "processing_id": command_processing_id,
+                    "ids_of_missing_roles": e.ids_of_missing_roles,
+                },
+            )
+            raise e
+        except WritersDoNotExistError as e:
+            logger.debug(
+                "Expected error occurred: "
+                "Writers ids entered by user do not belong to any writers",
+                extra={
+                    "processing_id": command_processing_id,
+                    "ids_of_missing_writers": e.ids_of_missing_writers,
+                },
+            )
+            raise e
+        except CrewMembersDoNotExistError as e:
+            logger.debug(
+                "Expected error occurred: "
+                "Crew members ids entered by user do not belong to any"
+                "crew members",
+                extra={
+                    "processing_id": command_processing_id,
+                    "ids_of_missing_crew_members": e.ids_of_missing_crew_members,
+                },
+            )
+            raise e
+        except PersonsDoNotExistError as e:
+            logger.debug(
+                "Expected error occurred: "
+                "Person ids entered by user do not belong to any persons",
+                extra={
+                    "processing_id": command_processing_id,
+                    "ids_of_missing_persons": e.ids_of_missing_persons,
+                },
+            )
+            raise e
+        except Exception as e:
+            logger.exception(
+                "Unexpected error occurred",
+                exc_info=e,
+                extra={
+                    "processing_id": command_processing_id,
+                    "error": e,
+                },
             )
             raise e
 
         logger.debug(
-            msg="Edit movie command was processed",
-            extra={"contribution_id": result},
+            "'Edit Movie' command processing completed",
+            extra={
+                "processing_id": command_processing_id,
+                "contribution_id": result,
+            },
         )
 
         return result
