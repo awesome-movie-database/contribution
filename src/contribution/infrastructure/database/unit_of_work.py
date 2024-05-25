7b82e269d213d68cea0341312789add9d2be3895
Madnoberson <baseddepartmentzx77@gmail.com>
2024-05-25 16:40:26 +0400

    Add `MongoDBUnitOfWork`

---
+++ src/contribution/infrastructure/database/unit_of_work.py
@@ -0,0 +1,130 @@
+# mypy: disable-error-code="arg-type"
+
+import copy
+from typing import Protocol, Sequence, Union
+
+from motor.motor_asyncio import AsyncIOMotorClientSession
+
+from contribution.domain import (
+    Movie,
+    User,
+    Person,
+    Role,
+    Writer,
+    CrewMember,
+    Contribution,
+    AddMovieContribution,
+    EditMovieContribution,
+    AddPersonContribution,
+    EditPersonContribution,
+    Achievement,
+)
+from contribution.infrastructure.database.collection_committers import (
+    CommitUserCollectionChanges,
+    CommitMovieCollectionChanges,
+    CommitPersonCollectionChanges,
+    CommitRoleCollectionChanges,
+    CommitWriterCollectionChanges,
+    CommitCrewMemberCollectionChanges,
+)
+
+
+type AnyEntity = Union[
+    Movie,
+    User,
+    Person,
+    Role,
+    Writer,
+    CrewMember,
+    Contribution,
+    AddMovieContribution,
+    EditMovieContribution,
+    AddPersonContribution,
+    EditPersonContribution,
+    Achievement,
+]
+
+
+class CommitCollectionChanges[E: AnyEntity](Protocol):
+    async def __call__(
+        self,
+        *,
+        new: Sequence[E],
+        clean: Sequence[E],
+        dirty: Sequence[E],
+        deleted: Sequence[E],
+    ) -> None:
+        raise NotImplementedError
+
+
+class MongoDBUnitOfWork:
+    def __init__(
+        self,
+        commit_user_collection_changes: CommitUserCollectionChanges,
+        commit_movie_collection_changes: CommitMovieCollectionChanges,
+        commit_person_collection_changes: CommitPersonCollectionChanges,
+        commit_role_collection_changes: CommitRoleCollectionChanges,
+        commit_writer_collection_changes: CommitWriterCollectionChanges,
+        commit_crew_member_collection_changes: CommitCrewMemberCollectionChanges,
+        session: AsyncIOMotorClientSession,
+    ):
+        self._collection_changes_commiters: dict[
+            type[AnyEntity],
+            CommitCollectionChanges,
+        ] = {
+            User: commit_user_collection_changes,
+            Movie: commit_movie_collection_changes,
+            Person: commit_person_collection_changes,
+            Role: commit_role_collection_changes,
+            Writer: commit_writer_collection_changes,
+            CrewMember: commit_crew_member_collection_changes,
+        }
+        self._session = session
+
+        self._new: dict[type[AnyEntity], dict[int, AnyEntity]] = {}
+        self._clean: dict[type[AnyEntity], dict[int, AnyEntity]] = {}
+        self._dirty: dict[type[AnyEntity], dict[int, AnyEntity]] = {}
+        self._deleted: dict[type[AnyEntity], dict[int, AnyEntity]] = {}
+
+    def register_new(self, entity: AnyEntity) -> None:
+        entity_id = id(entity)
+        self._new[type(entity)][entity_id] = entity
+
+    def register_clean(self, entity: AnyEntity) -> None:
+        entity_id = id(entity)
+        self._clean[type(entity)][entity_id] = copy.deepcopy(entity)
+
+    def register_dirty(self, entity: AnyEntity) -> None:
+        entity_id = id(entity)
+        if entity_id not in self._new[type(entity)]:
+            self._dirty[type(entity)][entity_id] = entity
+
+    def register_deleted(self, entity: AnyEntity) -> None:
+        entity_id = id(entity)
+
+        if entity_id in self._new[type(entity)]:
+            self._new[type(entity)].pop(entity_id)
+            return
+        if entity_id in self._dirty[type(entity)]:
+            self._clean[type(entity)].pop(entity_id)
+            self._dirty[type(entity)].pop(entity_id)
+
+        self._deleted[type(entity)][entity_id] = entity
+
+    async def commit(self) -> None:
+        entity_types = (
+            User,
+            Movie,
+            Person,
+            Role,
+            Writer,
+            CrewMember,
+        )
+        for entity_type in entity_types:
+            await self._collection_changes_commiters[entity_type](
+                new=self._new[entity_type].values(),
+                clean=self._clean[entity_type].values(),
+                dirty=self._dirty[entity_type].values(),
+                deleted=self._deleted[entity_type].values(),
+            )
+        await self._session.commit_transaction()
