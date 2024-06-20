813fcb4a13b83bebb34eda839862e85b0b8c936c
Madnoberson <baseddepartmentzx77@gmail.com>
2024-06-20 22:08:09 +0400

    Add docstring for `create_movie` cli command handler

--- src/contribution/presentation/cli/commands/movies.py
+++ src/contribution/presentation/cli/commands/movies.py
@@ -37,23 +37,90 @@ async def create_movie(
     duration: Annotated[int, Parameter("--duration")],
     budget: Annotated[
         Optional[Money],
-        Parameter("--budget", converter=json_to_money),
+        Parameter(
+            "--budget",
+            converter=json_to_money,
+            help=(
+                "Budget in json format \n"
+                'Example: {"amount": "100", "currency": "USD"}'
+            ),
+        ),
     ] = None,
     revenue: Annotated[
         Optional[Money],
-        Parameter("--budget", converter=json_to_money),
+        Parameter(
+            "--budget",
+            converter=json_to_money,
+            help=(
+                """
+                Revenue in json format.
+
+                Example: {"amount": "100", "currency": "USD"}
+                """
+            ),
+        ),
     ] = None,
     roles: Annotated[
-        list[MovieRole],
-        Parameter("--roles", converter=jsons_to_movie_roles),
+        Iterable[MovieRole],
+        Parameter(
+            "--roles",
+            converter=jsons_to_movie_roles,
+            help=(
+                """
+                List of roles in json format (Each role must be in json
+                format).
+
+                Example of a role:
+                {
+                    "id": "066746d0-a236-7f1d-8000-1711de4b4e03",
+                    "person_id": "066746c5-fda5-7985-8000-d16c225a8b17",
+                    "character": "Captain Jack Sparrow",
+                    "importance": 1,
+                    "is_spoiler": false
+                }
+                """
+            ),
+        ),
     ] = [],
     writers: Annotated[
-        list[MovieWriter],
-        Parameter("--writers", converter=jsons_to_movie_writers),
+        Iterable[MovieWriter],
+        Parameter(
+            "--writers",
+            converter=jsons_to_movie_writers,
+            help=(
+                """
+                List of writers in json format (Each writer must be in
+                json format).
+
+                Example of a writer:
+                {
+                    "id": "066746e6-b9b3-7b13-8000-120c5b23f268",
+                    "person_id": "066746e5-71e3-73e5-8000-d9bdd5ead582",
+                    "writing": "Origin"
+                }
+                """
+            ),
+        ),
     ] = [],
     crew: Annotated[
-        list[MovieCrewMember],
-        Parameter("--crew", converter=jsons_to_movie_crew),
+        Iterable[MovieCrewMember],
+        Parameter(
+            "--crew",
+            converter=jsons_to_movie_crew,
+            help=(
+                """
+                List of crew members in json format (Each crew member must be
+                in json format).
+
+                Example of a crew member:
+                {
+                    "id": "066746ff-367e-721f-8000-655eca7fdf18",
+                    "person_id": "066746fe-49a6-7e8d-8000-d780471a81f9",
+                    "membership": "Director"
+                }
+                """
+            ),
+        ),
     ] = [],
 ) -> None:
     ioc_container = ioc_container_factory()
