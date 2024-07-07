55dac7df70f19ed4a403dac37b063fed0c000e96
Madnoberson <baseddepartmentzx77@gmail.com>
2024-07-07 13:59:47 +0400

    Add `setup_routes`

--- src/contribution/presentation/web_api/__init__.py
+++ src/contribution/presentation/web_api/__init__.py
@@ -1,7 +1,7 @@
 __all__ = (
-    "router",
+    "setup_routes",
     "setup_exception_handlers",
 )
 
-from .router import router
+from .router import setup_routes
 from .exception_handlers import setup_exception_handlers
--- src/contribution/presentation/web_api/router.py
+++ src/contribution/presentation/web_api/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import FastAPI, APIRouter
 
 from .routers import (
     movie_contribution_requests_router,
@@ -6,7 +6,10 @@ from .routers import (
 )
 
 
-router = APIRouter(prefix="/v1")
+def setup_routes(app: FastAPI) -> None:
+    router = APIRouter(prefix="/v1")
 
-router.include_router(movie_contribution_requests_router)
-router.include_router(person_contribution_requests_router)
+    router.include_router(movie_contribution_requests_router)
+    router.include_router(person_contribution_requests_router)
+
+    app.include_router(router)
