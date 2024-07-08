c2f8f68a567798756cd8699d2952dbd809be8347
Madnoberson <baseddepartmentzx77@gmail.com>
2024-07-08 18:16:14 +0400

    Refactor domain services

--- src/contribution/domain/services/accept_contribution.py
+++ src/contribution/domain/services/accept_contribution.py
@@ -13,6 +13,8 @@ from contribution.domain.entities import (
 )
 
 
+INCREASE_RATING_ON = 10
+
 ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_1 = 1
 ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_2 = 10
 ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_3 = 100
@@ -20,7 +22,7 @@ ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_4 = 500
 ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_5 = 1000
 ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_6 = 10000
 
-ACCEPTED_CONTRIBUTIONS_MAPPED_ON_ACHIEVEMENT = {
+ACHIEVEMENTS = {
     ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_1: Achieved.ACCEPTED_CONTRIBUTIONS_1,
     ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_2: Achieved.ACCEPTED_CONTRIBUTIONS_2,
     ACCEPTED_CONTRIBUTIONS_FOR_ACHIEVEMENT_3: Achieved.ACCEPTED_CONTRIBUTIONS_3,
@@ -31,9 +33,6 @@ ACCEPTED_CONTRIBUTIONS_MAPPED_ON_ACHIEVEMENT = {
 
 
 class AcceptContribution:
-    def __init__(self, increase_rating_on: float):
-        self._increase_rating_on = increase_rating_on
-
     def __call__(
         self,
         *,
@@ -45,7 +44,7 @@ class AcceptContribution:
         contribution.status = ContributionStatus.ACCEPTED
         contribution.status_updated_at = current_timestamp
 
-        author.rating += self._increase_rating_on
+        author.rating += INCREASE_RATING_ON
         author.accepted_contributions_count += 1
 
         return self._try_earn_achievement(
@@ -61,7 +60,7 @@ class AcceptContribution:
         user: User,
         current_timestamp: datetime,
     ) -> Optional[Achievement]:
-        achieved = ACCEPTED_CONTRIBUTIONS_MAPPED_ON_ACHIEVEMENT.get(
+        achieved = ACHIEVEMENTS.get(
             user.accepted_contributions_count,
             None,
         )
