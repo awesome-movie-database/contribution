c214bd81d109d618e1e52eb41ad359a81b6596f2
Madnoberson <baseddepartmentzx77@gmail.com>
2024-07-10 09:29:26 +0400

    Add finalization for redis client

--- src/contribution/infrastructure/cache/redis_.py
+++ src/contribution/infrastructure/cache/redis_.py
@@ -1,10 +1,18 @@
+from typing import AsyncGenerator
+
 from redis.asyncio import Redis
 
 from .config import RedisConfig
 
 
-def redis_factory(redis_config: RedisConfig) -> Redis:
-    return Redis.from_url(
+async def redis_factory(
+    redis_config: RedisConfig,
+) -> AsyncGenerator[Redis, None]:
+    redis = Redis.from_url(
         url=redis_config.url,
         decode_responses=True,
     )
+    try:
+        yield redis
+    finally:
+        await redis.close()
