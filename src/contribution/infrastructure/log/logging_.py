f134772f75a8115aae15d8df6c9cff969f9a1d39
Madnoberson <baseddepartmentzx77@gmail.com>
2024-07-11 11:41:27 +0400

    Refactor `web_api_operation_id_factory` and logging

--- src/contribution/infrastructure/log/logging_.py
+++ src/contribution/infrastructure/log/logging_.py
@@ -4,13 +4,14 @@ from .json_formatter import JsonFormatter
 
 
 def setup_logging() -> None:
-    loggers = [
-        logging.getLogger(logger_name)
-        for logger_name in logging.root.manager.loggerDict
-        if logger_name.startswith(
-            "contribution.application.command_processors.",
-        )
-    ]
+    loggers: list[logging.Logger] = []
+    for logger_name in logging.root.manager.loggerDict:
+        if _is_command_processor_logger(
+            logger_name,
+        ) or _is_operation_id_factory_logger(logger_name):
+            logger = logging.getLogger(logger_name)
+            loggers.append(logger)
+
     for logger in loggers:
         logger.setLevel(logging.DEBUG)
 
@@ -18,3 +19,15 @@ def setup_logging() -> None:
         stream_handler.setFormatter(JsonFormatter())
 
         logger.addHandler(stream_handler)
+
+
+def _is_command_processor_logger(logger_name: str) -> bool:
+    return logger_name.startswith(
+        "contribution.application.command_processors.",
+    )
+
+
+def _is_operation_id_factory_logger(logger_name: str) -> bool:
+    return logger_name.startswith(
+        "contribution.infrastructure.operation_id.",
+    )
--- src/contribution/infrastructure/operation_id/web_api.py
+++ src/contribution/infrastructure/operation_id/web_api.py
@@ -1,12 +1,20 @@
+import logging
+
 from fastapi import Request
 
 from contribution.application import OperationId
 
 
+logger = logging.getLogger(__name__)
+
+
 def web_api_operation_id_factory(request: Request) -> OperationId:
-    operation_id_as_str = request.headers.get("X-Operation-Id", None)
+    operation_id_as_str = request.headers.get("X-Operation-Id")
     if not operation_id_as_str:
-        message = "Got no operation id for web api"
-        raise ValueError(message)
+        logger.error(
+            "Unexpected error occurred: Request has no operation id header",
+            extra={"request_headers": request.headers},
+        )
+        raise ValueError()
 
     return OperationId(operation_id_as_str)
