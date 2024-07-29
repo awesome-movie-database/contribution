90f3d9be63cd68eb486bb54dc037ba01547fbfa0
Madnoberson <baseddepartmentzx77@gmail.com>
2024-07-29 22:55:04 +0400

    Refactor docstring for `default_operation_id_factory`

--- src/contribution/infrastructure/operation_id/default.py
+++ src/contribution/infrastructure/operation_id/default.py
@@ -6,8 +6,8 @@ from contribution.application import OperationId
 def default_operation_id_factory() -> OperationId:
     """
     Factory of OperationId that should be used if
-    interactor call initializer is admin
+    initializer of interactor call is admin
     (for example if interactor was called from cli)
-    or no other operation was provided.
+    or no other operation id was provided.
     """
     return OperationId(uuid7().hex)
